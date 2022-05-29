53be7ef7fab1a93dbc565e94855ffa7c7c5dcfab
dev81log <dev81log@gmail.com>
2022-05-29 11:30:25 -0300

    Estudos formato Moedas em c#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+decimal contabilities = 12.65m;
+Console.WriteLine(contabilities);
+
+decimal contabilitiesCulture = 12065m;
+Console.WriteLine(
+    contabilitiesCulture.ToString(
+        "C", CultureInfo.CreateSpecificCulture("pt-BR")));
+
+Console.WriteLine("================");
+
+var mathRounding = 15987.56m;
+Console.WriteLine(Math.Round(mathRounding));
+Console.WriteLine(Math.Ceiling(mathRounding)); // (Ceiling up valor)
+Console.WriteLine(Math.Floor(mathRounding)); // (Floor up valor)
